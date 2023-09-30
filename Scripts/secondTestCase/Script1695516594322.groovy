32308cf8c6436ecc4569a730e76a48bccd5b3546
anji1 <anji1@host.docker.internal>
2023-09-30 22:30:43 +1300

    final commit

--- Scripts/secondTestCase/Script1695516594322.groovy
+++ Scripts/secondTestCase/Script1695516594322.groovy
@@ -17,3 +17,72 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demowebshop.tricentis.com/')
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop/a_Log in'))
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Login/input_Email_Email'), 'demoAutomarion@gmail.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Login/input_Password_Password'), 'HBiNh08voQs=')
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Login/input_Forgot password_button-1 login-button'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop/span_Shopping cart'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Shopping Cart/input_Remove_removefromcart'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Shopping Cart/div_I agree with the terms of service and I_c30d4c'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Shopping Cart/input_I agree with the terms of service and_f529a0'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Shopping Cart/button_Checkout'))
+
+WebUI.delay(5)
+
+WebUI.selectOptionByValue(findTestObject('Test - Copy (1)'), '3249660', false)
+
+WebUI.delay(20)
+
+WebUI.selectOptionByLabel(findTestObject('Test - Copy (1)'), 'New Address', false)
+
+WebUI.click(findTestObject('Test'))
+
+WebUI.click(findTestObject('Test - Copy'))
+
+WebUI.delay(3)
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_City_BillingNewAddress.City'), 
+    'NEW ZELAND')
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Address 1_BillingNewAddress.Address1'), 
+    '')
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Address 1_BillingNewAddress.Address1'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/select_Select countryUnited StatesCanadaAfg_dd1e54'), 
+    '55', true)
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Address 1_BillingNewAddress.Address1'), 
+    'TEST')
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Address 2_BillingNewAddress.Address2'), 
+    'RED')
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Zip  postal code_BillingNewAddress.Zi_77bd94'), 
+    '12345')
+
+WebUI.setText(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Phone number_BillingNewAddress.PhoneNumber'), 
+    '2345')
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Fax number_button-1 new-address-next-_2f210c'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input_Fax number_button-1 new-address-next-_2f210c'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/input__button-1 shipping-method-next-step-button'))
+
+WebUI.click(findTestObject('Object Repository/CartPage/Page_Demo Web Shop. Checkout/a_Log out'))
+
+WebUI.closeBrowser()
+
